2657b6ae2947e2fbb3d80191d6245e253a21507f
Fredy Gomes <fredyultima2@gmail.com>
2022-07-29 15:56:01 -0300

    Little improvement in board printing

--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -15,4 +15,8 @@ public class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
